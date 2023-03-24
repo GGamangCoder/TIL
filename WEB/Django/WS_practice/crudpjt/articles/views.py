225c0d45fda8c0db23fdbd089544f5048a0c7ca1
unknown <zxskmj@gmail.com>
2023-03-24 18:04:06 +0900

    crudpjt

---
+++ WEB/Django/WS_practice/crudpjt/articles/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from .models import Article
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    article = Article(title=title, content=content)
+    article.save()
+
+    return redirect('articles:detail', article.pk)
