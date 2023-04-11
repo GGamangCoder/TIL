58a8c71fb2076f6a728819c1676cfc0437b7db2d
unknown <zxskmj@gmail.com>
2023-04-11 15:46:45 +0900

    update crudpjt - ModelForm

--- WEB/Django/WS_practice/crudpjt/articles/views.py
+++ WEB/Django/WS_practice/crudpjt/articles/views.py
@@ -20,17 +20,28 @@ def detail(request, pk):
 
 def create(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-
-        article = Article(title=title, content=content)
-        article.save()
-
-        return redirect('articles:detail', article.pk)
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
     else:
         form = ArticleForm()
-        context = { 'form': form, }
-        return render(request, 'articles/create.html', context)
+
+    context = { 'form': form, }
+    return render(request, 'articles/create.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm(instance=article)
+
+    context = { 'form': form, 'article': article }
+    return render(request, 'articles/update.html', context)
 
 
 def delete(request, pk):
@@ -42,13 +53,3 @@ def delete(request, pk):
         return redirect('articles:detail', article.pk)
 
 
-def update(request, pk):
-    article = Article.objects.get(pk=pk)
-    if request.method == 'POST':
-        article.title = request.POST.get('title')
-        article.content = request.POST.get('content')
-        article.save()
-        return redirect('articles:detail', article.pk)
-    else:
-        context = { 'article': article }
-        return render(request, 'articles/update.html', context)
