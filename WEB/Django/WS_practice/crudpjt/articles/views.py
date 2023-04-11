33f086cfa27b18a41b0a791baa02da5aa5c25344
unknown <zxskmj@gmail.com>
2023-04-11 16:38:36 +0900

    update crudpjt - upload imagefile

--- WEB/Django/WS_practice/crudpjt/articles/views.py
+++ WEB/Django/WS_practice/crudpjt/articles/views.py
@@ -20,7 +20,7 @@ def detail(request, pk):
 
 def create(request):
     if request.method == 'POST':
-        form = ArticleForm(request.POST)
+        form = ArticleForm(request.POST, request.FILES)
         if form.is_valid():
             article = form.save()
             return redirect('articles:detail', article.pk)
--- WEB/Django/WS_practice/crudpjt/crud/urls.py
+++ WEB/Django/WS_practice/crudpjt/crud/urls.py
@@ -15,10 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-
-# (pjt)/urls.py
-# media file 관리
 from django.conf import settings
 from django.conf.urls.static import static
 
