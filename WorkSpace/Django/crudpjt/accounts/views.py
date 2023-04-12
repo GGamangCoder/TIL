ec92aabd808dbef4dd78cbe0a9612bee81dc4c32
unknown <zxskmj@gmail.com>
2023-04-12 16:37:47 +0900

    update login & logout

--- WorkSpace/Django/crudpjt/accounts/views.py
+++ WorkSpace/Django/crudpjt/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
 
 # Create your views here.
 def login(request):
@@ -15,3 +17,8 @@ def login(request):
         'form': form
     }
     return render(request, 'accounts/login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
+
