6d106386f0544eec7b5ac6711e5579e3b29a5483
unknown <zxskmj@gmail.com>
2023-04-12 16:55:34 +0900

    update signup setting but get error

--- WorkSpace/Django/crudpjt/accounts/views.py
+++ WorkSpace/Django/crudpjt/accounts/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
+# 인증과 권한 모델(폼)
 from django.contrib.auth.forms import AuthenticationForm
+# 로그인 / 로그아웃
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+# 회원가입 / 탈퇴
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
+# 로그인 / 로그아웃
 def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
@@ -22,3 +27,17 @@ def logout(request):
     auth_logout(request)
     return redirect('articles:index')
 
+
+# 회원 가입
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = UserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
