ad72e14fa0f926aa8826197fe5a4ad26d49366da
unknown <zxskmj@gmail.com>
2023-04-12 14:21:16 +0900

    setting for starting auth - accounts

--- WEB/Django/WS_practice/crudpjt/crud/settings.py
+++ WEB/Django/WS_practice/crudpjt/crud/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # 새로운 앱 추가
-    'articles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'articles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
