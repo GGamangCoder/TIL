10c6af4ccf080280eff5f7b81b739319372a42e1
zxskmj <zxskmj@gmail.com>
2023-10-23 15:40:14 +0900

    delete WorkSpace/

--- WorkSpace/Django/crudpjt/articles/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Article
-
-# Create your forms here.
-class ArticleForm(forms.ModelForm):
-
-    class Meta:
-        model = Article
-        fields = '__all__'
